c02591271ecee46ed711aecd8f70128f710bfd07
peijie <pjwqq@sohu.com>
2019-09-23 14:06:16 +0800

    chg

--- src/api/api.js
+++ src/api/api.js
@@ -4,3 +4,4 @@ export const orderreceive = params => { return axios.post(`/orderreceive`, param
 export const voyagelist = params => { return axios.post(`/voyagelist`, params); };
 export const imcontainerlist = params => { return axios.post(`/imcontainerlist`, params); };
 export const excontainerlist = params => { return axios.post(`/excontainerlist`, params); };
+export const getEmptyContainer = params => { return axios.post(`/emptycontainer`, params); };
--- src/views/customerquery/EmptyContainer.vue
+++ src/views/customerquery/EmptyContainer.vue
@@ -2,34 +2,27 @@
   <div style="height:60px;padding:10px;border:1px solid black;">
     <div style="overflow:hidden;text-align:left;">
       <span style="float:left;margin-top:15px;margin-left:120px;" class="title">输入箱主：</span>
-
       <a-input style="width: 80px;margin-top:14px;margin-left:5px;" size="small" v-model="lncd" />
-
       <span style="margin-top:15px;margin-left:20px;" class="title">输入箱型：</span>
-
       <a-input style="width: 80px;margin-top:14px;margin-left:5px;" size="small" v-model="ctty" />
-
-      <a-button style="margin-left:20px;" size="small">查找</a-button>
-
+      <a-button style="margin-left:20px;" @click="getData1()" size="small">查找</a-button>
       <a-button style="margin-left:20px;" size="small">下载</a-button>
     </div>
     <div style="margin-top:20px;padding:5px;border:1px solid black;">
-      <nbctCompactTable :columns="columns" :rows="ocs" :loading="loading" />
+      <nbctCompactTable :columns="columns" :rows="rows" :loading="loading" />
     </div>
   </div>
 </template>
+
 <script>
-import { mapState } from 'vuex';
-import moment from 'moment';
-import { getEmptyContainer } from '@/api/customerInfo';
-import xlsx from '@/utils/xlsx';
+import { getEmptyContainer } from '@/api/api';
 import nbctCompactTable from '@/components/NBCTCompactTable.vue';
 
 export default {
   data() {
     return {
-      lncd:"",
-      ctty:"",
+      lncd: 'CMA', //
+      ctty: 'GP', //
       columns: [
         {
           title: '箱号',
@@ -74,59 +67,31 @@ export default {
           key: 5
         }
       ],
-      tableData: []
+      rows: [] //
     };
-  },
-  computed: {
-    ...mapState({
-      // 动态主路由
-      userType: state => state.user.userType,
-      companyId: state => state.user.companyId,
-      companyHadCodes: state => state.user.companyHadCodes
-    })
   },
   components: {
     nbctCompactTable
   },
-  mounted() {
-    this.options = this.companyHadCodes;
-  },
   methods: {
-    handleSelChange(value) {
-      this.selValue = value;
-      console.log(`selected ${value}`);
-    },
-    getData() {
-
-
-          getEmptyContainer({
-            lncd:this.lncd,
-            ctty:this.ctty,
-          }).then(res => {
-            let { flag, data } = res;
-            if (flag) {
-        
+    getData1() {
+      getEmptyContainer({
+        lncd: this.lncd,
+        ctty: this.ctty
+      }).then(res => {
+        let { flag, data } = res;
+        if (flag) {
+          this.rows = data.map((item,index) =>{
             
-                data.forEach(element => {
-                  let tmp = {};
-                  tmp.cntrid = cntrid;
-                  tmp.vscd = vscd;
-                  tmp.vsvy = vsvy;
-                  tmp.vscd = vsdr;
-                  tmp.ctsz = ctsz;
-                  tmp.ctty = ctty;
-                  tmp.indm = indm;
-                  tmp.lncd = lncd;   
-                });
-              
+          });
         } else {
           this.$notification.error({
             message: '出错',
-            description: '请选择箱主'
+            description: ''
           });
         }
-    }),
+      });
+    }
   }
-  }
-}
+};
 </script>
